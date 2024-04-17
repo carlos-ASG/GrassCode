af530f998f9deb9bc7aad9460030750c548b7aca
YazminOlivares <146601100+YazminOlivares@users.noreply.github.com>
2024-04-16 23:13:01 -0700

    completar lenguaje

--- logica/lexico/lexer.py
+++ logica/lexico/lexer.py
@@ -13,15 +13,50 @@ tokens = [
 ]
 
 reservadas = [
-    'if', 
+    'if',
+    'else', 
     'while',
-    'switch'
+    'switch',
+    'fun',
+    'for',
+    'bring',
+    'try',
+    'catch',
+    'void',
+    'class',
+    'const',
+    'return',
+    'init',
+    'degree',
+    'time',
+    'distance',
+    'temp',
+    'bool',
+    'state',
+    'any',
+    'or',
+    'not',
+    'and'
 ]
 
 t_PARENTESIS_A = r"\("
 t_PARENTESIS_C = r"\)"
 t_LLAVE_A = r"\{"
 t_LLAVE_C = r"\}"
+t_CORIZQ = r'\['
+t_CORDER = r'\]'
+t_SUMA = r"\+"
+t_RESTA = r"\-"
+t_MULT = r'\*'
+t_DIV = r'/'
+t_MODULO = r'\%'
+t_POTENCIA = r'(\^)'
+t_ASIGNAR = r'='
+t_MENORQUE = r'<'
+t_MAYORQUE = r'>'
+t_PUNTOCOMA = ';'
+t_COMA = r','
+
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -32,6 +67,60 @@ def t_ID(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
+    return t
+
+def t_ENTERO(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_CADENA(t):
+   r'\"?(\w+ \ *\w*\d* \ *)\"?'
+   return t
+
+def t_SINO(t):
+    r'else'
+    return t
+
+def t_SI(t):
+    r'if'
+    return t
+
+def t_RETURN(t):
+   r'return'
+   return t
+
+def t_VOID(t):
+   r'void'
+   return t
+
+def t_MIENTRAS(t):
+    r'while'
+    return t
+
+def t_PARA(t):
+    r'for'
+    return t
+
+def t_MENORIGUAL(t):
+    r'<='
+    return t
+
+def t_MAYORIGUAL(t):
+    r'>='
+    return t
+
+def t_IGUAL(t):
+    r'=='
+    return t
+
+def t_MENOR(t):
+    r'<'
+    return t
+
+def t_MAYOR(t):
+    r'>'
+    return t
 
 t_ignore = ' \t'
 
