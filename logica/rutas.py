bba8971e175b83492c6830a647d233e5b3f5fd39
esl-carlos <carlossandoval@estadolimpio.com>
2024-05-01 16:45:20 -0700

    funcion para conectar el lexico con la interfaz lista

--- logica/rutas.py
+++ logica/rutas.py
@@ -1,8 +1,11 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, jsonify
+from lexico.lexer import analisis
 
 bp = Blueprint('rutas', __name__)
 
-@bp.route('/enviar')
-def enviar():
-    pass
+@bp.route('/lexico', methods=['POST'])
+def lexico():
+    codigo_fuente = request.json.get('codigo_fuente','')
+    tokens = analisis(codigo_fuente)
+    print(tokens)
+    return jsonify({'tokens': tokens})
