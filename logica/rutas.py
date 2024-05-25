b2f9766db85c2950216e4ac46021911b759d13c1
esl-carlos <carlossandoval@estadolimpio.com>
2024-05-25 12:56:27 -0700

    api con el sintactico

--- logica/rutas.py
+++ logica/rutas.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from lexico.lexer import analisis
+from lexico.sintactico import prueba_sintactica
 
 bp = Blueprint('rutas', __name__)
 
@@ -8,5 +9,10 @@ def lexico():
     codigo_fuente = request.json.get('codigo_fuente','')
     #print(codigo_fuente)
     tokens = analisis(codigo_fuente['codigo_fuente'])
-    print(tokens)
-    return jsonify({'tokens': tokens})
+    sintactico = prueba_sintactica(codigo_fuente['codigo_fuente'])
+    return jsonify({
+        'tokens': tokens,
+        'sintactico': sintactico,
+        })
+
+
